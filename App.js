1fb408a0fb8dc0388e56591099fe3b65fcdc5e4b
preksha <prekshajain1511@gmail.com>
2019-02-15 14:04:12 +0530

    react intro

---
+++ App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Display from './Display';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+        <div>
+      <div id ="head">
+        <h1>Hii, I am react app!!</h1>
+        </div>
+        <div id="app1">
+        <Display name='Preksha' age='20'/>
+        </div>
+        <div id="app2">
+        <Display name='Pooja' age='20'/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
